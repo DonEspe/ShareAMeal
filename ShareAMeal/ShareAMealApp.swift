acc05083c9ac38a78317031204ffbbf1e5b07f79
Don Espe <Don.Espe@gmail.com>
2023-10-17 00:53:19 -0500

    Initial Commit

---
+++ ShareAMeal/ShareAMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareAMealApp.swift
+//  ShareAMeal
+//
+//  Created by Don Espe on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ShareAMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
